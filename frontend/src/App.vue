82c401d2d069dbb9a228dc1cf38d646c81aa1ca3
Даниил Гузанов <daniilg13@mail.ru>
2026-08-21 04:34:18 +0300

    Добавлено переключение между просмотром статей и аналитикой

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <v-app>
+    <v-app-bar>
+      <v-app-bar-title>
+        Articles Test Case App
+      </v-app-bar-title>
+
+      <v-btn to="/articles">
+        Статьи
+      </v-btn>
+
+      <v-btn to="/analytics">
+        Аналитика
+      </v-btn>
+    </v-app-bar>
+    
     <v-main>
       <router-view />
     </v-main>
